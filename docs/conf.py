30599cda4204299627bb04d0772113b4c94ca5ad
calvinkim27 <39894172+calvinkim27@users.noreply.github.com>
2018-06-20 14:30:41 +0900

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -103,3 +103,20 @@ exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
+
